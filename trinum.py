3706fa4a97e7a2eb09762f855d31750aa847cc59
XaviLami <xavier.lami@edu.devinci.fr>
2021-04-22 16:52:45 -0400

    CSV:Tri de numéro de téléphone portable français

--- trinum.py
+++ trinum.py
@@ -1,23 +1,20 @@
-
 #Trier que les numéros de portable (690) et ne pas faire de doublon, puis re-sortir un fichier csv avec 1 seule colone 
 
 #Lecture des numéros du fichier CSV 
 import re
 import csv
-with open('phones.csv',newline='') as f:
-    reader = csv.reader(f)
+with open('phones.csv', newline='') as csvfile:
+    reader = csv.reader(csvfile, delimiter=';')
     for row in reader:
-        print(row)
-print(row.count("0590902549;"))     
-#result = re.search('^0590[0-9]*',row)
-"""
-if result  :
-    print("Match")      
-else : 
-    print("not match")
-"""
-#Tri des numéros 
+        #print(row)        
+        rowSum = row[0]+"\n"+row[1]
+        print(rowSum)
+        matches = re.search("^0590[0-9]*",rowSum)
+        if matches  :
+                print("Match")      
+        else : 
+            print("not match")
+#Tri des numéros   
 
 
 
-    
