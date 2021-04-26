5d546995ab6b36b57036d75a2c8c422753e030da
XaviLami <xavier.lami@edu.devinci.fr>
2021-04-26 12:14:54 -0400

    copylist : fonctionne partiellement,certains numéros coupés

--- trinum.py
+++ trinum.py
@@ -1,27 +1,30 @@
 #Trier que les numéros de portable (690) et ne pas faire de doublon, puis re-sortir un fichier csv avec 1 seule colonne 
-#new_arr = old_ arr
+#new_arr = old_ arr (a voir )
 #Trouver un moyen pour stocker les valeurs prises par itération sans les écrasser. 
+#Faire un tableau a l'exterieure de la boucle et inserer les valeurs a la voler ou Pandas
+#Utiliser la fonction 'write' du module csv pour créer mon nouveau csv 
 
 #Lecture des numéros du fichier CSV 
+
 import re
 import csv
 from copy import*
 with open('phones.csv', newline='') as csvfile:
     reader = csv.reader(csvfile, delimiter=';')
+    copyList = []
     for row in reader:
         #print(row)        
         rowSum = row[0]+"\n"+row[1]
-        print(row[0])
+        #print(row[0])
        
         matchesUn = re.search("0590[0-9]*",row[0])
-        matchesDeux = re.search("0590[0-9]*",row[1])
 
+        
         if matchesUn  :
-                print("Match")
-                copyList = copy(row[0])  
-        else : 
-         print("not match")
-print("\n"+copyList)
+               # print("Match")  
+                copyList.append(row[0])
+print(copyList)               
+
 #Tri des numéros   
 
 
